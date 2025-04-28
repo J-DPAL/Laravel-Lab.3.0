8e6a741c9305299072d4437cc670e29ce2d30c7c
Jean-David Pallares <jeandavid.pallares@gmail.com>
2025-04-28 10:30:57 -0400

    Last update

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Post;
 
 class Author extends Model
 {
     use HasFactory;
     public function posts()
-{
-    return $this->hasMany(Post::class);
-}
+    {
+        return $this->hasMany(Post::class);
+    }
 
 }
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Post;
+use App\Models\Author;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
